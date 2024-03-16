edb3ab914bd35e6052bf104ffd380a4685a96680
slrodini <rodini.simone.luigi@gmail.com>
2024-03-16 17:10:17 +0100

    Added rust directory

--- Code/compile.sh
+++ Code/compile.sh
@@ -1,2 +1,2 @@
 WAR_FLAGS="-Wall -Wextra -Wno-unused-result -Wno-unused-function"
-clang-16 -o main.elf main.c adam.c annealing.c minimize.c multilayer.c read_config.c utils.c ran2.c chi2.c -I./ -lm -fopenmp -march=native -ftree-vectorize -O3 ${WAR_FLAGS} -pg
+gcc-12 -o main.elf main.c adam.c annealing.c minimize.c multilayer.c read_config.c utils.c ran2.c chi2.c -I./ -lm -fopenmp -march=native -ftree-vectorize -O3 ${WAR_FLAGS} -pg
