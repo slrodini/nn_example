6dca060ab4b8ed3831c3aea266ddb64d6ddbd338
slrodini <rodini.simone.luigi@gmail.com>
2024-04-07 12:12:11 +0200

    Update

---
+++ Code/lecture_1/.ipynb_checkpoints/play_w_bits-checkpoint.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdint.h>
+
+#define S_MASK 0b10000000000000000000000000000000
+#define E_MASK 0b01111111100000000000000000000000
+#define M_MASK 0b00000000011111111111111111111111
+
+static void print_bin_int32_for_float(int32_t a)
+{
+   static char bin_rep[33] = {0};
+   for (int32_t i = 0; i < 32; i++) {
+      bin_rep[i] = ((a & (1 << (31 - i))) ? '1' : '0');
+   }
+   // divides sign bit, exponent (8 bits), mantissa (23 bits)
+   for (int32_t i = 0; i < 1; i++)
+      printf("%c", bin_rep[i]);
+   printf("|");
+   for (int32_t i = 1; i < 9; i++)
+      printf("%c", bin_rep[i]);
+   printf("|");
+   for (int32_t i = 9; i < 32; i++)
+      printf("%c", bin_rep[i]);
+   printf("|");
+   printf("\n");
+}
+
+void print_float(float x)
+{
+   int32_t a = *(int32_t *)&x;
+
+   int32_t sign = (S_MASK & a) >> (32 - 1);
+   int32_t exponent = ((E_MASK & a) >> (32 - 1 - 8)) - 127;
+   int32_t mantissa = (M_MASK & a);
+
+   printf("The original number: %f\nThe binary division of the number: ",  x);
+   print_bin_int32_for_float(a);
+   printf("The number representation: %s2^{%d} x (1", sign ? "-" : "+", exponent);
+   for (int32_t i=22; i>=0; i--){
+      if(mantissa & (1 << (i))){
+         printf(" + 2^{-%d}", 23-i);
+      }
+   }
+   printf(")\n");
+
+}
+
+int main()
+{
+   float x = 3;
+   print_float(x);
+}
