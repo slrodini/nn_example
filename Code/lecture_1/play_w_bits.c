6990c6911406eacddb2bee0774a1139dd56219ac
slrodini <rodini.simone.luigi@gmail.com>
2024-04-08 09:41:37 +0200

    Added Windows pre-compiled exe

--- Code/lecture_1/play_w_bits.c
+++ Code/lecture_1/play_w_bits.c
@@ -4,6 +4,14 @@
 #define S_MASK 0b10000000000000000000000000000000
 #define E_MASK 0b01111111100000000000000000000000
 #define M_MASK 0b00000000011111111111111111111111
+#define _EXIT_()                                                                                                                                     \
+   {                                                                                                                                                 \
+      int toExit = 0;                                                                                                                                \
+      while (1 != toExit) {                                                                                                                          \
+         fprintf(stderr, "Do you want to exit? 0: No, 1: Yes, >1: Undefined\n");                                                                     \
+         fscanf(stdin, "%d", &toExit);                                                                                                               \
+      }                                                                                                                                              \
+   }
 
 static void print_bin_int32_for_float(int32_t a)
 {
@@ -32,20 +40,26 @@ void print_float(float x)
    int32_t exponent = ((E_MASK & a) >> (32 - 1 - 8)) - 127;
    int32_t mantissa = (M_MASK & a);
 
-   printf("The original number: %f\nThe binary division of the number: ",  x);
+   printf("The original number: %f\nThe binary division of the number: ", x);
    print_bin_int32_for_float(a);
    printf("The number representation: %s2^{%d} x (1", sign ? "-" : "+", exponent);
-   for (int32_t i=22; i>=0; i--){
-      if(mantissa & (1 << (i))){
-         printf(" + 2^{-%d}", 23-i);
+   for (int32_t i = 22; i >= 0; i--) {
+      if (mantissa & (1 << (i))) {
+         printf(" + 2^{-%d}", 23 - i);
       }
    }
    printf(")\n");
-
 }
 
 int main()
 {
    float x = 3;
+
+   fprintf(stderr,"Please input your floating point number: \t");
+   fscanf(stdin, "%f", &x);
+   
    print_float(x);
+
+   _EXIT_();
+   return 0;
 }
